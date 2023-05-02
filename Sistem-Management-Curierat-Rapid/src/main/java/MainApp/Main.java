fa65c01bae6dc3690ebc336e0db25b1c95f37bbc
Dumitru Ursu <dumitru.ursu555@gmail.com>
2023-05-02 22:56:16 +0300

    swing gui test

---
+++ Sistem-Management-Curierat-Rapid/src/main/java/MainApp/Main.java
@@ -0,0 +1,13 @@
+package MainApp;
+
+import javax.swing.*;
+
+public class Main {
+    private JPanel panel1;
+    private JLabel text;
+    private JButton button1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}

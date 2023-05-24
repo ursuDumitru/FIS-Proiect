b7812a0158d78b49a00b304222c772229cae9f9f
DanMelinte <Dmelinte2595@gmail.com>
2023-05-24 19:02:39 +0300

    1

---
+++ Sistem-Management-Curierat-Rapid/src/main/java/Clase/Sofer.java
@@ -0,0 +1,59 @@
+package Clase;
+
+public class Sofer {
+    private int idSofer;
+    private String numeSofer;
+    private String pozitieActuala;
+    private String oraActualizare;
+
+    public Sofer() {}
+
+    public Sofer(int idSofer, String numeSofer, String pozitieActuala, String oraActualizare) {
+        this.idSofer = idSofer;
+        this.numeSofer = numeSofer;
+        this.pozitieActuala = pozitieActuala;
+        this.oraActualizare = oraActualizare;
+    }
+
+    public int getIdSofer() {
+        return idSofer;
+    }
+
+    public void setIdSofer(int idSofer) {
+        this.idSofer = idSofer;
+    }
+
+    public String getNumeSofer() {
+        return numeSofer;
+    }
+
+    public void setNumeSofer(String numeSofer) {
+        this.numeSofer = numeSofer;
+    }
+
+    public String getPozitieActuala() {
+        return pozitieActuala;
+    }
+
+    public void setPozitieActuala(String pozitieActuala) {
+        this.pozitieActuala = pozitieActuala;
+    }
+
+    public String getOraActualizare() {
+        return oraActualizare;
+    }
+
+    public void setOraActualizare(String oraActualizare) {
+        this.oraActualizare = oraActualizare;
+    }
+
+    @Override
+    public String toString() {
+        return "Sofer{" +
+                "idSofer=" + idSofer +
+                ", numeSofer='" + numeSofer + '\'' +
+                ", pozitieActuala='" + pozitieActuala + '\'' +
+                ", oraActualizare='" + oraActualizare + '\'' +
+                '}';
+    }
+}
